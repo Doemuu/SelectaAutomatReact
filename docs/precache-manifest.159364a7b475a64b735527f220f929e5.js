994a6479b264042c557dcda2c487a339196f7fe2
Doemuu <43069409+Doemuu@users.noreply.github.com>
2020-01-22 12:05:56 +0100

    build react app

---
+++ docs/precache-manifest.159364a7b475a64b735527f220f929e5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a827b4b94850e63718cb0708ea09a973",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b19555016cabfd20ddb5",
+    "url": "/static/css/2.545c6387.chunk.css"
+  },
+  {
+    "revision": "9bdbe1659586c324b56e",
+    "url": "/static/css/main.0c83cf77.chunk.css"
+  },
+  {
+    "revision": "b19555016cabfd20ddb5",
+    "url": "/static/js/2.940e606c.chunk.js"
+  },
+  {
+    "revision": "9bdbe1659586c324b56e",
+    "url": "/static/js/main.e581a229.chunk.js"
+  },
+  {
+    "revision": "70e702b4efc5b8867b8e",
+    "url": "/static/js/runtime-main.19e53192.js"
+  },
+  {
+    "revision": "dbb125767490aadb1553b6953a930c1d",
+    "url": "/static/media/coke.dbb12576.png"
+  },
+  {
+    "revision": "1b3063966e9ed69c893996f7bfc52613",
+    "url": "/static/media/fr_0.1.1b306396.png"
+  },
+  {
+    "revision": "ef04a10ef58ac4a242c9b601c3f3eecc",
+    "url": "/static/media/fr_0.2.ef04a10e.png"
+  },
+  {
+    "revision": "bb1211c1cbd39d3adf8b498136fd0767",
+    "url": "/static/media/fr_0.5.bb1211c1.png"
+  },
+  {
+    "revision": "076b45ba2b58f24f9261c86253904a79",
+    "url": "/static/media/fr_1.076b45ba.png"
+  },
+  {
+    "revision": "cef498d8d08ff56e73fbb01719005b5e",
+    "url": "/static/media/fr_2.cef498d8.png"
+  },
+  {
+    "revision": "5a4480182b5bcd27c28f712f810d88ba",
+    "url": "/static/media/fr_5.5a448018.png"
+  },
+  {
+    "revision": "c4221d57cfddb03316d3edeb631bceb5",
+    "url": "/static/media/nestea.c4221d57.png"
+  }
+]);
